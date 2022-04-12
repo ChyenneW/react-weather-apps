f06dc434a22ea1caf5c115a4f42a63d25f51258f
ChyenneW <williamscheyenne771@gmail.com>
2022-04-11 19:09:00 -0500

    Installed axios and Bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Weather App</div>
+      </header>
+    </div>
+  );
+}
