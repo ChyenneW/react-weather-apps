f4f54eb5ff55da656a954109059bb8aafda82b88
ChyenneW <williamscheyenne771@gmail.com>
2022-04-18 22:43:14 -0500

    Update HourlyForecast.js

--- src/HourlyForecast.js
+++ src/HourlyForecast.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import WeatherForecastDay from "./WeatherForecastDay";
 import HourlyForecastHour from "./HourlyForecastHour";
 
 export default function HourlyForecast(props) {
