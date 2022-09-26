416f821488789a699efbd335ae33a43afc79a14f
ChyenneW <williamscheyenne771@gmail.com>
2022-09-26 17:09:03 -0500

    Use new Date to form date

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,11 +1,33 @@
 import React from "react";
 
-export default function Date() {
-    let days = ["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"];
+export default function DateInfo() {
+    let now = new Date();
 
-    let months = ["January", "Feburary", "March", "April", "May", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"];
+    let day = days[now.getDay()];
 
+    const months = ["January", "Feburary", "March", "April", "May", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[now.getMonth()];
+    let date = now.getDate();
 
+    let hour = now.getHours();
+    if (hour <= 9) {
+        hour = `0${hour}`;
+    } else {
+        hour = hour - 12;
+    }
 
-    return <div>sunday,month day at time</div>
+    let minutes = now.getMinutes();
+    if (minutes <= 9) {
+        minutes = `0${minutes}`;
+    }
+
+    let sentence = `${day}, ${month} ${date} at ${hour}:${minutes}`;
+
+    if (now.getHours >= 12) {
+        return <div>{sentence} P.M</div>
+    } else {
+        return <div>{sentence} A.M</div>
+    }
+
 }
