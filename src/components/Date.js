e527558681cf1f49cdcf51fab892e3c3b38c1a58
ChyenneW <williamscheyenne771@gmail.com>
2022-09-26 16:26:00 -0500

    Create Date.js

---
+++ src/components/Date.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Date() {
+    let days = ["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "Feburary", "March", "April", "May", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
+    return <div>sunday,month day at time</div>
+}
