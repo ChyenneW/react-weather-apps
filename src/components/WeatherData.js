19d4f1dff68ed8723529e4e6005aebaa4f9fd9a3
ChyenneW <williamscheyenne771@gmail.com>
2022-09-26 17:21:23 -0500

    Create WeatherData.js

---
+++ src/components/WeatherData.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+export default function WeatherData() {
+
+    return (
+        <div className="details">
+            <div>description</div>
+            <div>humidity</div>
+            <div>wind</div>
+        </div>
+    )
+}
