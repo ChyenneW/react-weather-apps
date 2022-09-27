ac1314e03f4b6c5defa55585c86fae66c7f3b60f
ChyenneW <williamscheyenne771@gmail.com>
2022-09-27 01:47:13 -0500

    Update city with dispatch

--- src/components/SearchWeather.js
+++ src/components/SearchWeather.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/WeatherContext";
 
 import "./SearchWeather.css";
 
 export default function SearchWeather() {
+    const { contextCity, dispatch } = useContext(AppContext);
+    let [city, setCity] = useState("");
+
+    const submitCity = (event) => {
+        event.preventDefault();
+
+        dispatch({
+            type: 'UPDATE_CITY',
+            payload: city,
+        });
+    };
+
     return (
         <div className="searchArea">
-            <form >
+            <form onSubmit={submitCity}>
                 <input
                     type="search"
                     placeholder="Enter a City"
                     className="searchBar"
+                    onChange={(event) => setCity(event.target.value)}
                 />
                 <button
                     type="submit"
