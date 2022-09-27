0b861cb876b1b11049428c5fed7ee67840fba487
ChyenneW <williamscheyenne771@gmail.com>
2022-09-27 01:48:05 -0500

    Display city from appcontext

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import DateInfo from "./Date";
+import WeatherData from "./WeatherData";
+import { AppContext } from "../context/WeatherContext";
 
 import "./CurrentWeather.css";
 
 export default function CurrentWeather() {
+    const { city } = useContext(AppContext);
+
     return (
         <div className="descriptionBox">
             <div className="current">
-                <h1 id="currentCity">City</h1>
+                <h1 id="currentCity">{city}</h1>
                 <div className="currentTemp">
                     <span>74</span><span>F</span>|<span>C</span>
                 </div>
@@ -20,6 +24,7 @@ export default function CurrentWeather() {
                         <div>humidity</div>
                         <div>wind</div>
                     </div>
+                    <WeatherData />
                 </div>
                 <div id="weatherImage">weather image</div>
             </div>
