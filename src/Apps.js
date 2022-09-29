c7315901c0e7faf54d613007aade596fdffd97bd
ChyenneW <williamscheyenne771@gmail.com>
2022-09-28 19:35:25 -0500

    Link footer to app

--- src/Apps.js
+++ src/Apps.js
@@ -2,16 +2,20 @@ import SearchWeather from "./components/SearchWeather";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Footer from "./components/Footer";
 
 
 export default function Apps() {
     return (
-        <div className="d-flex align-items-center justify-content-center">
-            <div className="App">
-                <div>
-                    <SearchWeather />
+        <div>
+            <div className="d-flex align-items-center justify-content-center">
+                <div className="App">
+                    <div>
+                        <SearchWeather />
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
     );
 }
