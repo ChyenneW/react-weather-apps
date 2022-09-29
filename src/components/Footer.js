c20b1f9c324f5694dcde7bc7afc5fc5526519b8b
ChyenneW <williamscheyenne771@gmail.com>
2022-09-28 19:34:52 -0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <span><a href="https://github.com/ChyenneW/react-weather-apps" className="footerLink">Open-source code</a></span>
+            by
+            <span><a href="https://chyennew.dev" className="footerLink">Chyenne Williams</a></span>
+        </footer>
+    )
+}
