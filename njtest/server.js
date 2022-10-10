a6993b1c8a9a0f9d2cf095c404b09947053882ca
ChyenneW <williamscheyenne771@gmail.com>
2022-10-09 20:45:46 -0500

    Create json file fore njtest

--- njtest/server.js
+++ njtest/server.js
@@ -1,11 +1,27 @@
-/* load the HTTP library */
-var http = require('http');
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
 
-/* create an HTTP server to handle responses */
-http
-    .createServer(function (request, response) {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello World');
-        response.end();
+const app = express();
+
+app.post('/login', (reg, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'ca2f77c2d53d499eb58ebf742d9fddb2',
+        clientSecret: 'fa41901b5efa4c5ba8c5fca8ead611e8'
     })
-    .listen(8888);
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            })
+        })
+        .catch(() => {
+            res.sendStatus(400)
+        })
+
+})
