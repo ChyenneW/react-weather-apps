7e58cda5a5d3abfc30d4763a28e5c863baa994cd
ChyenneW <williamscheyenne771@gmail.com>
2022-10-18 12:13:33 -0500

    Add a timeout to that refreshes before expires in time

--- src/components/useAuth.js
+++ src/components/useAuth.js
@@ -21,14 +21,19 @@ export default function useAuth(code) {
 
     useEffect(() => {
         if (!refreshToken || !expiresIn) return
-        axios.post('http://localhost:3001/refresh', {
-            refreshToken,
-        }).then(res => {
-            setAccessToken(res.data.accessToken);
-            setExpiresIn(res.data.expiresIn);
-        }).catch(() => {
-            window.location = '/'
-        })
+
+        const timeout = setTimeout(() => {
+            axios.post('http://localhost:3001/refresh', {
+                refreshToken,
+            }).then(res => {
+                setAccessToken(res.data.accessToken);
+                setExpiresIn(res.data.expiresIn);
+            }).catch(() => {
+                window.location = '/'
+            })
+        }, (expiresIn - 60) * 1000)
+
+        return () => clearTimeout(timeout)
     }, [refreshToken, expiresIn])
 
     return accessToken
