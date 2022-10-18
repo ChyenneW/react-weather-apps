17a8f586de7f2825d15d15c318e9e4f438e0ae5a
ChyenneW <williamscheyenne771@gmail.com>
2022-10-18 11:48:46 -0500

    Update SpotifyPlayer.js

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -1,8 +1,11 @@
 import React from "react";
+import useAuth from "./useAuth";
 
-export default function SpotifyPlayer() {
+export default function SpotifyPlayer({ code }) {
+    const accessToken = useAuth(code)
     return (
         <div>
+            <div>{code}</div>
             <div>Music is playing</div>
         </div>
     )
