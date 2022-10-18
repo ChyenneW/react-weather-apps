165315930ab8e163090ebef9612f85d6abfc15e2
ChyenneW <williamscheyenne771@gmail.com>
2022-10-18 17:27:07 -0500

    Console search results for spotify api

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -1,4 +1,4 @@
-import React, { useEffet } from "react";
+import React, { useEffect } from "react";
 import useAuth from "./useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
 
@@ -6,17 +6,36 @@ const spotifyApi = new SpotifyWebApi({
     clientId: 'ca2f77c2d53d499eb58ebf742d9fddb2',
 })
 
-export default function SpotifyPlayer({ code }) {
+export default function SpotifyPlayer({ code }, { description }) {
     const accessToken = useAuth(code);
+    const playlistTopic = description;
 
     useEffect(() => {
         if (!accessToken) return
         spotifyApi.setAccessToken(accessToken)
     }, [accessToken])
 
+    useEffect(() => {
+        if (!playlistTopic) return
+        if (!accessToken) return
+
+        spotifyApi.searchPlaylists(playlistTopic)
+            .then(function (data) {
+                console.log('Found playlists are', data.body);
+            }, function (err) {
+                console.log('Something went wrong!', err);
+            });
+
+    }, [playlistTopic, accessToken])
+
+
     return (
         <div>
             <div>Music is playing</div>
+            <div>artist</div>
+            <div>song</div>
+            <div>album art</div>
+            <div>controls</div>
         </div>
     )
 }
