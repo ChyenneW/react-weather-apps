3424bd33a3671a04dc45737788adf96f3d95993a
ChyenneW <williamscheyenne771@gmail.com>
2022-10-18 11:47:02 -0500

    Update SpotifyLogin.js

--- src/components/SpotifyLogin.js
+++ src/components/SpotifyLogin.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 
+
 const CLIENT_ID = 'ca2f77c2d53d499eb58ebf742d9fddb2';
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-read-playback-state`;
 const CLIENT_SECRET = 'fa41901b5efa4c5ba8c5fca8ead611e8';
 const REDIRECT_URI = 'http://localhost:3000';
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-read-playback-state`;
 
 
 export default function SpotifyLogin() {
