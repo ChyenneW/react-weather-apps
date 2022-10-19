bdc39ceab85d0b66dafa03789f39877fbc8eb32c
ChyenneW <williamscheyenne771@gmail.com>
2022-10-18 20:37:05 -0500

    Play a song based on weather description

--- src/components/MySpotifyPlayer.js
+++ src/components/MySpotifyPlayer.js
@@ -3,6 +3,8 @@ import useAuth from "./useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
 import SpotifyPlayer from "react-spotify-web-playback";
 
+import "./MySpotifyPlayer.css";
+
 const spotifyApi = new SpotifyWebApi({
     clientId: 'ca2f77c2d53d499eb58ebf742d9fddb2',
 })
@@ -11,6 +13,10 @@ export default function MySpotifyPlayer(props) {
     const accessToken = useAuth(props.code);
     console.log(props.search);
     const playlistTopic = props.search;
+
+    let [trackUri, setTrackUri] = useState('');
+    let [artist, setArtist] = useState('');
+    let [title, setTitle] = useState('');
 
     useEffect(() => {
         if (!accessToken) return
@@ -21,6 +27,10 @@ export default function MySpotifyPlayer(props) {
         spotifyApi.searchTracks(playlistTopic)
             .then(function (data) {
                 console.log('found', data.body);
+                setTrackUri(data.body.tracks.items[0].uri);
+                setArtist(data.body.tracks.items[0].artists[0].name);
+                setTitle(data.body.tracks.items[0].name);
+
             }, function (err) {
                 console.log('Something went wrong!', err);
             });
@@ -29,17 +39,14 @@ export default function MySpotifyPlayer(props) {
 
 
     return (
-        <div>
-            <div>{playlistTopic}</div>
-            <div>artist</div>
-            <div>song</div>
-            <div>art</div>
-            <div>controls</div>
-            <SpotifyPlayer
-                token={accessToken}
-                showSaveIcon
-                uris={trackUri ? [trackUri] : []}
-            />
+        <div className="trackArea">
+            <div className="trackPlayer">
+                <SpotifyPlayer
+                    token={accessToken}
+                    showSaveIcon
+                    uris={trackUri ? [trackUri] : []}
+                />
+            </div>
         </div>
     )
 }
