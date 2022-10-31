b8a7adb1d9fa7d102bc19a8199a9f2348b81b6ef
ChyenneW <williamscheyenne771@gmail.com>
2022-10-31 14:13:24 -0500

    Update SpotifyLogin.js

--- src/components/SpotifyLogin.js
+++ src/components/SpotifyLogin.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const CLIENT_ID = 'ca2f77c2d53d499eb58ebf742d9fddb2';
-const REDIRECT_URI = 'http://localhost:3000';
+const REDIRECT_URI = 'https://react-spotify-weatherapp.netlify.app/';
 const SCOPES = 'streaming%20user-read-email%20user-read-private%20user-read-playback-state%20user-library-read%20user-library-modify%20user-modify-playback-state';
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}`;
 
